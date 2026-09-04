661e83772aa8284854a05d7a2f24d29b8279e8fa
Mehdi <m.abbasi7596@gmail.com>
2026-09-04 16:01:27 +0330

    Add viewcomponent for lastblog and most visit blog

---
+++ Aspdotnetkar/Components/LastBlogComponent.cs
@@ -0,0 +1,28 @@
+﻿using Aspdotnetkar.Context;
+using Aspdotnetkar.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aspdotnetkar.Components
+{
+    public class LastBlogComponent : ViewComponent
+    {
+        SiteContext _context;
+        public LastBlogComponent(SiteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var Mostvisite = _context.blogs.OrderByDescending(b => b.BlogVisitCount).Take(10).ToList();
+            var LastBlog = _context.blogs.OrderByDescending(o => o.BlogCreateDate).Take(10).ToList();
+
+            var vm = new BlogViewModel()
+            {
+                blogvm = LastBlog
+            };
+
+            return View("/Views/ComponentViews/LastandMostvisitBlog.cshtml", vm);
+        }
+    }
+}
